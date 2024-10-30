0caf4e73debdbcd5daabb5549fb154565904d122
djsumin <judy3504@naver.com>
2024-10-30 19:52:26 +0900

    create project

---
+++ kotlin-jwt-project/src/main/kotlin/org/project/kotlinjwtproject/KotlinJwtProjectApplication.kt
@@ -0,0 +1,11 @@
+package org.project.kotlinjwtproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinJwtProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinJwtProjectApplication>(*args)
+}
---
+++ kotlin-jwt-project/src/test/kotlin/org/project/kotlinjwtproject/KotlinJwtProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.project.kotlinjwtproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJwtProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
