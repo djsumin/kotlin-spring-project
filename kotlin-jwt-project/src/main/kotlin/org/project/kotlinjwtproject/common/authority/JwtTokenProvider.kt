1a39fe693dcf89c7da6887d6eb28ee9bf4c71ca2
djsumin <judy3504@naver.com>
2024-11-04 22:46:03 +0900

    made jwt token

--- kotlin-jwt-project/src/main/kotlin/org/project/kotlinjwtproject/common/authority/JwtTokenProvider.kt
+++ kotlin-jwt-project/src/main/kotlin/org/project/kotlinjwtproject/common/authority/JwtTokenProvider.kt
@@ -1,13 +1,16 @@
 package org.project.kotlinjwtproject.common.authority
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.*
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import jakarta.websocket.Decoder
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -47,4 +50,53 @@ class JwtTokenProvider {
 
 
     }
+
+
+    /*
+    * Token 정보 추출
+    * */
+    fun getAuthentication(token: String): Authentication {
+        val claims: Claims = getClaims(token)
+
+        val auth = claims["auth"]?: throw RuntimeException("잘못된 토큰입니다.")
+
+        //권한 정보 추출
+        val authorities: Collection<GrantedAuthority> = (auth as String)
+            .split(",")
+            .map {SimpleGrantedAuthority(it)}
+
+        val principal: UserDetails = User(claims.subject, "", authorities)
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    /*
+    * Token 검증
+    * */
+    fun validateToken(token: String): Boolean {
+        try{
+            getClaims(token)
+            return true
+        }catch (e: Exception){
+            when(e){
+                is SecurityException -> {}
+                is MalformedJwtException -> {}
+                is ExpiredJwtException -> {}
+                is UnsupportedJwtException -> {}
+                is IllegalArgumentException -> {}
+                else -> {}
+            }
+            println(e.message)
+        }
+        return false
+    }
+
+
+    private fun getClaims(token: String): Claims
+    = Jwts.parserBuilder()
+        .setSigningKey(key)
+        .build()
+        .parseClaimsJwt(token)
+        .body
+
+
 }
